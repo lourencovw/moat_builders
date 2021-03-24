392fa4ba830038b12bd90a13b056476f90afea4f
Lourenco Werneck <lourencovw@gmail.com>
2021-03-24 09:33:45 +0000

    fix(navigation-blade): fix nav links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,16 +4,15 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
-                </div>
+                <x-application-logo></x-application-logo>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('artists.create')" :active="request()->routeIs('artists.create')">
+                        {{ __('Artist List') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('albums.create')" :active="request()->routeIs('albums.create')">
+                        {{ __('Album List') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -38,8 +37,7 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log out') }}
                             </x-dropdown-link>
@@ -63,8 +61,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('artists.create')" :active="request()->routeIs('artists.create')">
+                {{ __('Artist List') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('albums.create')" :active="request()->routeIs('albums.create')">
+                {{ __('Album List') }}
             </x-responsive-nav-link>
         </div>
 
@@ -88,8 +89,7 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log out') }}
                     </x-responsive-nav-link>
